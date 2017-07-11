4a1e129a0be4aef9b4abd4d3ba9d5dec2bc9e164
Tyler Krupicka <tylerkrupicka@gmail.com>
2017-07-10 23:54:21 -0700

    add buttons to links and fix mixed formatting

--- src/views/Recent.js
+++ src/views/Recent.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Button} from 'react-native';
 import Expo from 'expo';
 import { Ionicons } from '@expo/vector-icons';
-import { Card, CardTitle, CardContent } from 'react-native-card-view';
+import { Card, CardTitle, CardContent, CardAction } from 'react-native-card-view';
 import Autolink from 'react-native-autolink';
 import { colors } from '../utils/constants';
 import jimmify from '../utils/jimmify';
 import RequestList from './shared/RequestList';
 
 const styles = {
-    item: {
-        question: {
-            fontWeight: 'bold',
-            color: colors.GRAY_DARK
-        },
-        answer: {
-            fontStyle: 'italic'
-        },
-				link: {
-					color: colors.BLUE
-				}
-    }
+	item: {
+		question: {
+			fontWeight: 'bold',
+			color: colors.GRAY_DARK
+		},
+		answer: {
+			fontStyle: 'italic'
+		},
+		button: {
+			 marginRight: 10
+		}
+	}
 };
 
 function openLink(link) {
@@ -28,35 +28,38 @@ function openLink(link) {
 }
 
 function RecentItem(props) {
-    const { item } = props;
-    return (
-        <Card>
-            <CardTitle styles={{ cardTitle: { paddingBottom: 0 } }}>
-                <Autolink style={styles.item.question} text={item.text}/>
-            </CardTitle>
-            <CardContent>
-                <Autolink style={styles.item.answer} text={item.answer} />
-            </CardContent>
-						{
-							item.list
-							? item.list.map((link) => {
-								return (<Text key={link} style={styles.item.link} onPress={openLink.bind(this, link)}>{link}</Text>)
-							})
-							: null
-						}
-        </Card>
-    );
+	const { item } = props;
+	return (
+		<Card>
+			<CardTitle styles={{ cardTitle: { paddingBottom: 0 } }}>
+				<Autolink style={styles.item.question} text={item.text}/>
+			</CardTitle>
+			<CardContent>
+				<Autolink style={styles.item.answer} text={item.answer} />
+			</CardContent>
+			<CardAction >
+			{
+				item.list
+				? item.list.map((link, index) => {
+					return (<Button key={link} style={styles.item.button} onPress={openLink.bind(this, link)} title={"Link " + (index + 1)}></Button>)
+				})
+				: null
+			}
+			</CardAction>
+
+		</Card>
+	);
 }
 
 const Wrapped =  RequestList(jimmify.getRecent, RecentItem, {
-    responseKey: 'recents'
+	responseKey: 'recents'
 });
 
 Wrapped.navigationOptions = {
-    tabBarLabel: 'Recent',
-    tabBarIcon: ({tintColor}) => {
-        return <Ionicons name="ios-book-outline" size={32} color={tintColor}/>
-    }
+	tabBarLabel: 'Recent',
+	tabBarIcon: ({tintColor}) => {
+		return <Ionicons name="ios-book-outline" size={32} color={tintColor}/>
+	}
 };
 
 export default Wrapped;
